65efc7a48a70c0eb4af37988f9d759e539d41b82
Vadim <vkamosko@yandex.by>
2021-01-26 12:01:23 +0300

    'init:finish'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (mas) {
+   if(!mas) return [];
+   let i=0;
+    for (val of mas)
+    {
+        if(i%2==1)
+        {
+            val.reverse();
+        }
+        i++;
+    }
+    return mas.flat();
 }
